4f434b21b80cb37f407604e593603ab8ab825260
angelsmithlgs <angelsmithlgs@gmail.com>
2023-10-19 12:26:31 -0300

    edit login

--- src/components/user/LoginForm.jsx
+++ src/components/user/LoginForm.jsx
@@ -1,88 +1,85 @@
 import React, { useState } from 'react';
+import './loginform.css';
 
 export default function LoginForm() {
-  const [selectedOption, setSelectedOption] = useState('registro');
+  const [selectedOption, setSelectedOption] = useState(null); // Inicializado en null
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
   };
 
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-6'>
+    <div className='loginform-container'>
+      <div className='loginform-row'>
+        <div className='loginform-col-md-6'>
           <div className='text-center'>
             <h1>
               {selectedOption === 'registro' ? 'Registro' : 'Iniciar Sesión'}
             </h1>
           </div>
-          <form method='post' action='index.html'>
-            <div className='mb-3'>
-              {selectedOption === 'registro' && (
-                <>
-                  <label htmlFor='name' className='form-label'>
-                    Nombre
-                  </label>
-                  <input type='text' className='form-control' id='name' />
-                </>
-              )}
-              <label htmlFor='email' className='form-label'>
-                Correo Electrónico
-              </label>
-              <input type='email' className='form-control' id='email' />
-            </div>
-            {selectedOption === 'registro' && (
+          <div className='loginform-col-md-6-options loginform-custom-option'>
+            <ul>
+              <li onClick={() => handleOptionChange('registro')}>
+                <button
+                  type='button'
+                  className='loginform-custom-btn loginform-custom-btn-registro'
+                >
+                  Registro
+                </button>
+              </li>
+              <li onClick={() => handleOptionChange('inicioSesion')}>
+                <button
+                  type='button'
+                  className='loginform-custom-btn loginform-custom-btn-inicio-sesion'
+                >
+                  Iniciar Sesión
+                </button>
+              </li>
+            </ul>
+          </div>
+          {selectedOption && (
+            <form method='post' action='index.html'>
               <div className='mb-3'>
-                <label htmlFor='username' className='form-label'>
-                  Username
+                {selectedOption === 'registro' && (
+                  <>
+                    <label htmlFor='name' className='form-label'>
+                      Nombre
+                    </label>
+                    <input type='text' className='custom-input' id='name' />
+                  </>
+                )}
+                <label htmlFor='email' className='form-label'>
+                  Correo Electrónico
                 </label>
-                <div className='input-group'>
-                  <span className='input-group-text' id='at-symbol'>
-                    @
-                  </span>
-                  <input type='text' className='form-control' id='username' />
-                </div>
+                <input type='email' className='custom-input' id='email' />
               </div>
-            )}
-            <div className='mb-3'>
-              <label htmlFor='password' className='form-label'>
-                Contraseña
-              </label>
-              <input type='password' className='form-control' id='password' />
-            </div>
-            <button type='submit' className='btn btn-primary'>
-              {selectedOption === 'registro' ? 'Registrarse' : 'Entrar'}
-            </button>
-          </form>
+              {selectedOption === 'registro' && (
+                <div className='mb-3'>
+                  <label htmlFor='username' className='form-label'>
+                    Username
+                  </label>
+                  <div className='custom-input-group'>
+                    <span className='custom-input-group-text' id='at-symbol'>
+                      @
+                    </span>
+                    <input type='text' className='custom-input' id='username' />
+                  </div>
+                </div>
+              )}
+              <div className='mb-3'>
+                <label htmlFor='password' className='form-label'>
+                  Contraseña
+                </label>
+                <input type='password' className='custom-input' id='password' />
+              </div>
+              <button type='submit' className='custom-button'>
+                {selectedOption === 'registro' ? 'Registrarse' : 'Entrar'}
+              </button>
+            </form>
+          )}
         </div>
         <div className='col-md-6'>
-          <div className='text-center'>
-            <h1>Selecciona una opción</h1>
-            <div className='btn-group' role='group' aria-label='Opciones'>
-              <input
-                type='radio'
-                id='radioRegistro'
-                name='opcion'
-                value='registro'
-                checked={selectedOption === 'registro'}
-                onChange={handleOptionChange}
-              />
-              <label htmlFor='radioRegistro' className='btn btn-secondary'>
-                Registro
-              </label>
-              <input
-                type='radio'
-                id='radioInicioSesion'
-                name='opcion'
-                value='inicioSesion'
-                checked={selectedOption === 'inicioSesion'}
-                onChange={handleOptionChange}
-              />
-              <label htmlFor='radioInicioSesion' className='btn btn-secondary'>
-                Iniciar Sesión
-              </label>
-            </div>
-          </div>
+          {/* Contenido de la columna de la imagen */}
         </div>
       </div>
     </div>
