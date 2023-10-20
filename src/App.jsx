6b45b0c195ebc2cf253e83b0112b8a47756f0ac3
angelsmithlgs <angelsmithlgs@gmail.com>
2023-10-19 21:52:58 -0300

    update form regis/login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/landing/Landing';
-import LoginForm from './components/user/LoginForm';
 import Store from './components/tienda/Ecommerce';
 import NotFound from './components/error/Error404';
 import Comentarios from './admin/Comentarios';
+import RegisForm from './components/user/RegisForm';
+import LoginForm from './components/user/LoginForm';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Landing />} />
-        <Route path='/login' element={<LoginForm />} />
         <Route path='/tienda' element={<Store />} />
+        <Route path='/loginform' element={<LoginForm />} />
+        <Route path='/regisform' element={<RegisForm />} />
         <Route path='*' element={<NotFound />} />{' '}
         <Route path='/comentarios' element={<Comentarios />} />
       </Routes>
