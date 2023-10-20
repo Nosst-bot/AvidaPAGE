84ef671bc584dc6e9fd9a2751d6e95c29d8289ca
Kevin <kevsalvatierrafs@gmail.com>
2023-10-20 01:46:37 -0300

    Corregidas las clases del footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,56 +7,56 @@ import imgface from './footerimg/facebook.png';
 
 const Footer = () => {
   return (
-    <div class='container-footer'>
-      <footer class='row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top'>
-        <div class='col mb-3'>
-          <img src={logo} alt='Logo-Ávida' class='logo' />
+    <div className='container-footer'>
+      <footer className='row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top'>
+        <div className='col mb-3'>
+          <img src={logo} alt='Logo-Ávida' className='logo' />
         </div>
-        <div class='col mb-3'></div>
-        <div class='col mb-3'>
-          <ul class='nav flex-column'>
-            <li class='nav-item mb-2'>
-              <a href='#' class='nav-link p-0 text-body-secondary'>
+        <div className='col mb-3'></div>
+        <div className='col mb-3'>
+          <ul className='nav flex-column'>
+            <li className='nav-item mb-2'>
+              <a href='#' className='nav-link p-0 text-body-secondary'>
                 Inicio
               </a>
             </li>
-            <li class='nav-item mb-2'>
-              <a href='#' class='nav-link p-0 text-body-secondary'>
+            <li className='nav-item mb-2'>
+              <a href='#' className='nav-link p-0 text-body-secondary'>
                 Mapa
               </a>
             </li>
-            <li class='nav-item mb-2'>
-              <a href='#' class='nav-link p-0 text-body-secondary'>
+            <li className='nav-item mb-2'>
+              <a href='#' className='nav-link p-0 text-body-secondary'>
                 Comunidad
               </a>
             </li>
           </ul>
         </div>
 
-        <div class='col mb-3'>
-          <ul class='nav flex-column'>
-            <li class='nav-item mb-2'>
-              <a href='#' class='nav-link p-0 text-body-secondary'>
+        <div className='col mb-3'>
+          <ul className='nav flex-column'>
+            <li className='nav-item mb-2'>
+              <a href='#' className='nav-link p-0 text-body-secondary'>
                 Guía
               </a>
             </li>
-            <li class='nav-item mb-2'>
-              <a href='#' class='nav-link p-0 text-body-secondary'>
+            <li className='nav-item mb-2'>
+              <a href='#' className='nav-link p-0 text-body-secondary'>
                 Tienda
               </a>
             </li>
-            <li class='nav-item mb-2'>
-              <a href='#' class='nav-link p-0 text-body-secondary'>
+            <li className='nav-item mb-2'>
+              <a href='#' className='nav-link p-0 text-body-secondary'>
                 Mi cuenta
               </a>
             </li>
           </ul>
         </div>
 
-        <div class='col mb-3'>
-          <img src={imgface} alt='Logo-Facebook' class='icono_footer' />
-          <img src={imginsta} alt='Logo-Instagram' class='icono_footer' />
-          <img src={imgemail} alt='Logo-email' class='icono_footer' />
+        <div className='col mb-3'>
+          <img src={imgface} alt='Logo-Facebook' className='icono_footer' />
+          <img src={imginsta} alt='Logo-Instagram' className='icono_footer' />
+          <img src={imgemail} alt='Logo-email' className='icono_footer' />
         </div>
       </footer>
     </div>
