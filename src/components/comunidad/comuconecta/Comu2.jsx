3872afda43eea264004b6527f29cf2442acafb0a
angelsmithlgs <angelsmithlgs@gmail.com>
2023-10-19 23:22:56 -0300

    R fixed + P Csection

---
+++ src/components/comunidad/comuconecta/Comu2.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './comu2.css';
+
+const Comu2 = () => {
+  return (
+    <div className='container container-espacio'>
+      <div className='row'>
+        <div className='col-md-6 flex-column justify-content-center'>
+          <h1 className='conecta-title mb-3 align-self-start'>
+            Explora con nosotros
+          </h1>
+          <p className='conecta-text text-muted align-self-start'>
+            En Ávida, valoramos profundamente nuestros ecosistemas. Te instamos
+            a unirte a nosotros para preservar estos tesoros naturales. Al
+            explorar nuestras rutas, te pedimos que veas más allá de tus huellas
+            y consideres el impacto que tenemos en la tierra. Los ecosistemas
+            son esenciales para nuestra vida y la vida silvestre. Al cuidar
+            nuestras rutas, escribimos una historia de conservación y aseguramos
+            un hogar para todas las criaturas. Únete a nuestra misión para
+            proteger nuestro planeta.
+          </p>
+        </div>
+        <div className='col-md-6'></div>
+      </div>
+      <div className='section-divider'></div>
+    </div>
+  );
+};
+
+export default Comu2;
--- src/components/landing/Comuland.jsx
+++ src/components/landing/Comuland.jsx
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 import Comu1 from '../comunidad/comuhome/Comu1';
 import Header from '../header/Header';
+import Comu2 from '../comunidad/comuconecta/Comu2';
 
 function Comuland() {
   return (
     <div className='app-container'>
       <Header />
       <Comu1 />
+      <Comu2 />
     </div>
   );
 }
